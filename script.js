0269166894cf6bf6e055909949161923a6e8bcf3
sushmoyr <sushmoyr@gmail.com>
2022-01-25 18:48:48 +0600

    html ui

---
+++ script.js
@@ -0,0 +1,11 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select a media stream, pass to video element and play
+async function selectMediaStream() {
+    try {
+
+    } catch (e) {
+
+    }
+}
